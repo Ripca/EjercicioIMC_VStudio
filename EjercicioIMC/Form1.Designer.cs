b644ed71837aac5552c080dcc9b64e1897744d5a
Ripca <117389221+Ripca@users.noreply.github.com>
2023-11-22 15:51:50 -0600

    Add project files.

---
+++ EjercicioIMC/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace EjercicioIMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEstatura = new System.Windows.Forms.Label();
+            this.txtEstatura = new System.Windows.Forms.TextBox();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.dgvIMS = new System.Windows.Forms.DataGridView();
+            this.lblResultado = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvIMS)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblEstatura
+            // 
+            this.lblEstatura.AutoSize = true;
+            this.lblEstatura.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstatura.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblEstatura.Location = new System.Drawing.Point(121, 78);
+            this.lblEstatura.Name = "lblEstatura";
+            this.lblEstatura.Size = new System.Drawing.Size(82, 19);
+            this.lblEstatura.TabIndex = 0;
+            this.lblEstatura.Text = "Estatura(m):";
+            // 
+            // txtEstatura
+            // 
+            this.txtEstatura.Location = new System.Drawing.Point(209, 77);
+            this.txtEstatura.Name = "txtEstatura";
+            this.txtEstatura.Size = new System.Drawing.Size(71, 20);
+            this.txtEstatura.TabIndex = 1;
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeso.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblPeso.Location = new System.Drawing.Point(122, 132);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(64, 19);
+            this.lblPeso.TabIndex = 2;
+            this.lblPeso.Text = "Peso(kg):";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.IndianRed;
+            this.lblTitulo.Location = new System.Drawing.Point(144, 27);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(172, 21);
+            this.lblTitulo.TabIndex = 3;
+            this.lblTitulo.Text = "¡CALCULA TU IMC!";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(209, 132);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(71, 20);
+            this.txtPeso.TabIndex = 4;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(326, 108);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // dgvIMS
+            // 
+            this.dgvIMS.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgvIMS.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvIMS.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvIMS.Location = new System.Drawing.Point(23, 200);
+            this.dgvIMS.Name = "dgvIMS";
+            this.dgvIMS.Size = new System.Drawing.Size(503, 106);
+            this.dgvIMS.TabIndex = 6;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblResultado.Location = new System.Drawing.Point(165, 167);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 19);
+            this.lblResultado.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 331);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.dgvIMS);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.txtEstatura);
+            this.Controls.Add(this.lblEstatura);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvIMS)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEstatura;
+        private System.Windows.Forms.TextBox txtEstatura;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.DataGridView dgvIMS;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ EjercicioIMC/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjercicioIMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int contador=0;
+            contador++;
+
+            for (int i = 0; i < contador; i++)
+            {
+                double Estatura, Peso, IMC;
+            string rangoDePeso="", imcRango="", NivelDePeso="";
+
+            Estatura=double.Parse(txtEstatura.Text);
+            Peso = double.Parse(txtPeso.Text);
+
+            IMC = Math.Round(Peso / (Math.Pow(Estatura, 2)),2);
+
+            lblResultado.Text = "Tu imc es: " + IMC;
+
+            if (IMC<18.5)
+            {
+                rangoDePeso = "124 lb o menos";
+                imcRango = "Por debajo de 18.5";
+                NivelDePeso = "Bajo peso";
+            }
+            else if (IMC>=18.5 && IMC<=24.9)
+            {
+                rangoDePeso = "125 a 168 lb";
+                imcRango = "18.5 a 24.9 ";
+                NivelDePeso = "Normal";
+            }
+            else if (IMC >= 25 && IMC <= 29.9)
+            {
+                rangoDePeso = "169 lb a 202 lb";
+                imcRango = "25.0 a 29.9";
+                NivelDePeso = "Sobrepaso";
+            }
+            else if (IMC >= 30)
+            {
+                rangoDePeso = "203 lb o mas";
+                imcRango = "30 o más";
+                NivelDePeso = "Obesidad";
+            }
+
+            //Creacion de tabla para luego colocarse al DataGridView
+
+
+                DataTable tabla = new DataTable();
+            tabla.Columns.Add("Estatura", typeof(string));
+            tabla.Columns.Add("Rango de peso", typeof(string));
+            tabla.Columns.Add("IMC", typeof(string));
+            tabla.Columns.Add("Nivel de peso", typeof(string));
+
+            tabla.Rows.Add(Estatura+" m",rangoDePeso,imcRango,NivelDePeso);
+            dgvIMS.DataSource = tabla;
+            }
+
+
+        }
+    }
+}
